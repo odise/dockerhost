f982345bc8064f9625b630057186029f05e928d9
Jan Nabbefeld <jan.nabbefeld@kreuzwerker.de>
2015-05-07 10:43:53 +0200

    logspout start and env test

--- cookbooks/example_host/metadata.rb
+++ cookbooks/example_host/metadata.rb
@@ -2,7 +2,7 @@ name             'example_host'
 maintainer       'Jan Nabbefeld'
 maintainer_email 'jan.nabbefeld@kreuzwerker.de'
 license          'All rights reserved'
-version          '0.1.2'
+version          '0.1.3'
 
 depends 'dockerhost'
 depends 'container'
--- cookbooks/example_host/recipes/default.rb
+++ cookbooks/example_host/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: cookbooks/example_host
 # Recipe:: default
 #
-# Copyright (C) 2015 YOUR_NAME
+# Copyright (C) 2015 Jan Nabbefeld
 #
 # All rights reserved - Do Not Redistribute
 #
@@ -15,7 +15,7 @@ if ( deploy? 'test_app' )
   container_unit 'container-test' do
     depend "docker"
     image "odise/busybox-curl"
-    command "/bin/sh -c 'while true; do echo xxx; sleep 2; done'"
+    command "/bin/sh -c 'while true; do echo ${VAR1} and ${VAR2}; sleep 2; done'"
     environment [
       "VAR1=VALUE1",
       "VAR2=VALUE2"
@@ -26,8 +26,14 @@ if ( deploy? 'test_app' )
     depend "docker"
     image "odise/busybox-curl"
     link "--link container-test:test"
-    ports "-p 8001:8001"
     command "/bin/sh -c 'while true; do echo zzz; sleep 2; done'"
+  end
+
+  container_unit 'logspout' do
+    depend "docker"
+    volumes "-v /var/run/docker.sock:/tmp/docker.sock"
+    ports "--publish=8000:8000"
+    image "gliderlabs/logspout"
   end
 
   container_unit 'container-test' do
@@ -38,19 +44,8 @@ if ( deploy? 'test_app' )
     action :restart
   end
 
-  #container_unit 'container-test' do
-  #  action :stop
-  #end
-
-  #container_unit 'container-dependency' do
-  #  action :remove
-  #end
-
   container_unit 'logspout' do
-    depend "docker"
-    volumes "-v /var/run/docker.sock:/tmp/docker.sock"
-    ports "--publish=8000:8000"
-    image "gliderlabs/logspout"
+    action :restart
   end
 
 end
