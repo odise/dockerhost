f9784b9808c97a9e726aeb2e602718204d9bce33
Jan Nabbefeld <jan.nabbefeld@kreuzwerker.de>
2015-05-07 10:11:10 +0200

    bump version of example

--- cookbooks/example_host/metadata.rb
+++ cookbooks/example_host/metadata.rb
@@ -2,7 +2,7 @@ name             'example_host'
 maintainer       'Jan Nabbefeld'
 maintainer_email 'jan.nabbefeld@kreuzwerker.de'
 license          'All rights reserved'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'dockerhost'
 depends 'container'
